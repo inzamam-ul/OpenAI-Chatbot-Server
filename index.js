f52bc6974c834749223bc729f5a265f03c0dc6af
unknown <inzamam.cu@gmail.com>
2023-05-04 12:46:09 +0800

    max_tokan incresed

--- index.js
+++ index.js
@@ -20,15 +20,14 @@ app.post("/chat", async (req, res) => {
       model: "text-davinci-003",
       prompt: "How to cure a plant disease?",
       temperature: 0,
-      max_tokens: 150,
-      // top_p: 1,
-      // frequency_penalty: 0.5,
-      // presence_penalty: 0,
-      // stop: ["You:"],
+      max_tokens: 1024,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+      stop: ["You:"],
     });
 
     // Send the generated response back to the client
-    console.log(response.data.choices);
     res.json(response.data.choices[0].text);
   } catch (error) {
     console.error(error);
